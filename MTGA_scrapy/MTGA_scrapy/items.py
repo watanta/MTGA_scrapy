275081d12f86a5d1a2a613b06bd4215aef47ca44
watanabe <watanabe101watanabe@yahoo.co.jp>
2019-02-07 01:24:21 +0900

    雛形完成

---
+++ MTGA_scrapy/MTGA_scrapy/items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MtgaScrapyItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    theme = scrapy.Field()
+    deckname = scrapy.Field()
+    main = scrapy.Field()
+    side = scrapy.Field()
+    date = scrapy.Field()
+
+
+
+    pass
---
+++ MTGA_scrapy/MTGA_scrapy/spiders/mtga_spider.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from scrapy import Request
+from MTGA_scrapy.items import MtgaScrapyItem
+
+
+class MtgaSpiderSpider(scrapy.Spider):
+    name = 'mtga_spider'
+    #allowed_domains = ['mtgdecks.net/Standard']
+    allowed_domains = []
+    start_urls = ['https://mtgdecks.net/Standard/']
+
+    def parse(self, response):
+        #crawl
+        item = MtgaScrapyItem()
+        themes = response.xpath('//*[@id="archetypesTable"]/tbody//td[2]/strong/a/@href').getall()
+        for theme in themes:
+            item["theme"] = theme.split('/')[2]
+            url = 'https://mtgdecks.net/Standard/' + item["theme"]
+            request = Request(url, callback=self.decks_by_theme)
+            request.meta["item"] = item
+
+            yield request
+
+    def decks_by_theme(self, response):
+        decks = response.xpath('//*[@id="content"]/div[3]/div[1]/div/div[1]/div/div/div/table//td[2]/a')
+        dates_path = response.xpath('//*[@id="content"]/div[3]/div[1]/div/div[1]/div/div/div/table//td[5]/strong')
+        for deck, date_path in zip(decks, dates_path):
+            item = response.meta["item"]
+            item["deckname"] = deck.xpath('text()').get()
+            url = 'https://mtgdecks.net/' + deck.xpath('@href').get()
+            month_day = date_path.xpath('text()[1]').get()
+            year = date_path.xpath('span/text()').get()
+            item['date'] = month_day + year
+            request = Request(url, callback=self.deck_detail)
+            request.meta["item"] = item
+
+            yield request
+
+    def deck_detail(self, response):
+        item = response.meta["item"]
+
+        creatures = response.xpath('//*[@id="compact"]/div/div/table[1]/tr[1]/following-sibling::tr/td[1]')
+        creature_dict = {}
+        for creature in creatures:
+            creature_name = creature.xpath('a/text()').get()
+            creature_amount = creature.xpath('text()[2]').get()[1]
+            creature_dict[creature_name] = creature_amount
+
+        instants = response.xpath('//*[@id="compact"]/div/div/table[2]/tr[1]/following-sibling::tr/td[1]')
+        instants_dict = {}
+        for instant in instants:
+            instant_name = instant.xpath('a/text()').get()
+            instant_amount = instant.xpath('text()[2]').get()[1]
+            instants_dict[instant_name] = instant_amount
+
+        scorcerys = response.xpath('//*[@id="compact"]/div/div/table[3]/tr[1]/following-sibling::tr/td[1]')
+        scorcery_dict = {}
+        for scorcery in scorcerys:
+            scorcery_name = scorcery.xpath('a/text()').get()
+            scorcery_amount = scorcery.xpath('text()[2]').get()[1]
+            scorcery_dict[scorcery_name] = scorcery_amount
+
+        enchantments = response.xpath('//*[@id="compact"]/div/div/table[4]/tr[1]/following-sibling::tr/td[1]')
+        enchantment_dict = {}
+        for enchantment in enchantments:
+            enchantment_name = enchantment.xpath('a/text()').get()
+            enchantment_amount = enchantment.xpath('text()[2]').get()[1]
+            enchantment_dict[enchantment_name] = enchantment_amount
+
+        planeswalkers = response.xpath('//*[@id="compact"]/div/div/table[5]/tr[1]/following-sibling::tr/td[1]')
+        planeswalker_dict = {}
+        for planeswalker in planeswalkers:
+            planeswalker_name = planeswalker.xpath('a/text()').get()
+            planeswalker_amount = planeswalker.xpath('text()[2]').get()[1]
+            planeswalker_dict[planeswalker_name] = planeswalker_amount
+
+        lands = response.xpath('//*[@id="compact"]/div/div/table[6]/tr[1]/following-sibling::tr/td[1]')
+        land_dict = {}
+        for land in lands:
+            land_name = land.xpath('a/text()').get()
+            land_amount = land.xpath('text()[2]').get()[1]
+            land_dict[land_name] = land_amount
+
+        sideboards = response.xpath('//*[@id="compact"]/div/div/table[7]/tr[1]/following-sibling::tr/td[1]')
+        sideboard_dict = {}
+        for sideboard in sideboards:
+            sideboard_name = sideboard.xpath('a/text()').get()
+            sideboard_amount = sideboard.xpath('text()[2]').get()[1]
+            sideboard_dict[sideboard_name] = sideboard_amount
+
+        main_deck = {}
+        main_deck.update(creature_dict)
+        main_deck.update(instants_dict)
+        main_deck.update(enchantment_dict)
+        main_deck.update(planeswalker_dict)
+        main_deck.update(land_dict)
+
+        item["main"] = main_deck
+        item["side"] = sideboard_dict
+
+        print(item)
+
+        yield item
